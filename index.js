67c85bf35d80a71f36ebf558976e9960a3793b0c
Daniel Palmer <daniel.palmer@shaw.ca>
2021-11-22 18:14:02 -0700

    Created base register page
    Ported the register page into the project, and depending files have been created, or modified.
    - register.html
    - register.css
    - readform.js
    - bouncer.php
    - index.js
    - gator.png

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+/**
+ * Index.js
+ * Serves as the main file for our project.
+ * Author: Daniel Palmer
+ * 2021-11-22
+ */
 const express = require("express");
 const app = express();
 
 var port = 8000;
 
+
+app.use(express.static("scripts"))
+app.use(express.static("views", {"extensions": ["html", "htm"]}))
+app.use(express.static("assets"))
+app.use(express.static("styles"))
+
 app.listen(port, ()=>{
-    console.log(`Server started on port ${port}, url: http://localhost:8000/${port}`)
+    console.log(`Server started on port ${port}, url: http://localhost:${port}`)
+});
+
+app.get('/register', (req, res)=>{
+    res.sendFile("register")
 });
