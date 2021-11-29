0580d1f1218d8fc970b2b0fce48aa82024fbfee3
Daniel Palmer <daniel.palmer@shaw.ca>
2021-11-29 10:03:19 -0700

    Register Function and Documentation
    Added the base for register validation + database insertion.
    
    -thankyou.ejs
    -addpassword.js (For development purposes, will be removed before live build)

---
+++ scripts/addpassworld.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql")
+
+//this page is solely for testing/database modification, and will not be in the final
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "travelexperts"
+});
+
+function signOn(email, password){
+    con.connect((err)=>{
+        if (err) throw err;
+        var sql = `SELECT password FROM customers WHERE CustEmail="${email}"`
+        con.query(sql, (err, results)=>{
+            if (err) throw err;
+            if (password == results[0].password){
+                console.log("Log in successfull")
+            }else{
+                console.log("Email and/or password are incorrect")
+            }
+            
+        });
+        con.end()
+    })
+}
+
+signOn("daniel.palmer@shaw.ca", "testword")
--- scripts/readform.js
+++ scripts/readform.js
@@ -1,6 +1,14 @@
+/**
+ * Index.js
+ * Serves as the main file for our project.
+ * Author: Daniel Palmer
+ * CPRG 207 - Threaded Project
+ * 2021-11-29
+ */
+
+//Pre-defined regex expressions for value validation
 var postalCheck = /(^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$)|(^\d{5}-\d{4}$)|(^\d{5}$)/
 var phoneCheck = /(^\d{10}$)|(^\(\d{3}\)-\d{3}-\d{4}$)|(^\d{3}-\d{3}-\d{4}$)/
-
 
 function checkValue(value, reg){
     if (reg.test(value)){
@@ -10,6 +18,14 @@ function checkValue(value, reg){
     }
 }
 
+/**
+ * Below is the form validation.
+ * First confirming all fields are filled
+ * Second verifying the password and confirm password are the same
+ * Third verifies postal code / zip code is formated properly
+ * Forth verifies that the phone number is formated properly
+ * Last, and ALWAYS LAST, the page requests confirmation to submit information to the server.
+ */
 function validate(){
     var elements = document.getElementById("registerform").elements;
     var form = document.forms[0]
@@ -22,21 +38,22 @@ function validate(){
         };
     };
     if (filledFields != 0){
-        alert(filledFields + " required fields are missing.")
+        alert(filledFields + " required fields are missing.");
         return false;
     }else if (form.password.value != form.cpassword.value){
-        alert("Password and Confirm Password do not match!")
+        alert("Password and Confirm Password do not match!");
         return false;
     }else if(!checkValue(form.postalcode.value, postalCheck)){
-        alert("Invalid Postal Code!")
+        alert("Invalid Postal Code!");
         return false;
     }else if(!checkValue(form.phonenumber.value, phoneCheck)){
-        console.log(form.phonenumber.value)
+        alert("Invalid Phone Number or Format");
+        return false;
+    }else if(!checkValue(form.busphone.value, phoneCheck)){
         alert("Invalid Phone Number or Format")
-        return false
+        return false;
     }else{
-        confirm("Confirm and Submit information?")
-        console.log(values)
+        confirm("Confirm and Submit information?");
     };
 };
 
