f31d954c33e1cc33a89b4d5943d4f4c7b33db2f9
Marat Nikitin <maratnik1@gmail.com>
2021-12-04 15:01:29 -0700

    dateFormatting.js was expanded, now for expired dates we have bold red text highlighting.

--- scripts/dateFormatting.js
+++ scripts/dateFormatting.js
@@ -1,16 +1,40 @@
 /**
  * dateFormatting.js
- * Helps to show date-time in meaningful way
+ * Helps to show date-time in meaningful way & highlight expired packages' start/end dates 
  * Author: Marat Nikitin
+ * Co-author: Daniel Palmer
  * CPRG 207 - Threaded Project
  * 2021-12-02
  */
 
- const dayjs = require("dayjs")
+ const dayjs = require("dayjs");
  const advancedFormat = require('dayjs/plugin/advancedFormat');
  dayjs.extend(advancedFormat);
-  
- exports.dateFormatting = (requestedDate)=>{
+ 
+ /* Below we set the date-time format for the Packages page */
+ function dateFormatting(requestedDate){
      return dayjs(requestedDate).format('dddd MMMM D, YYYY, h:mm a');
  };
  
+ /* Below we set different text colours and boldness for dates based on if they 
+      are in the future (green colour) or the past (bold red color) */
+ function dateColour(dateTested) {
+    var dateTested = Date.parse(dateTested);
+    var dateDifference = (dateTested - Date.now())/(1000*86400);
+    var dateColourValue;
+    var boldnessValue;
+    if (dateDifference > 0) {
+       dateColourValue = "green";
+       boldnessValue = "400";
+    }
+    else {
+       dateColourValue = "red";
+       boldnessValue = "bold";
+    };  
+    return [dateColourValue, boldnessValue];
+    /* Two values need to be returned, therefore we use an array */
+ };
+ 
+ exports.formattedDate = (requestedDate)=>{
+   return [dateFormatting(requestedDate), dateColour(requestedDate)[0], dateColour(requestedDate)[1]];
+ };
